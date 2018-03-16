cc7847ce9e86a59dbb2022bfad220c34b86bc577
Manacloud <isitamomster@gmail.com>
2018-03-16 22:26:47 +0200

    v0.2

--- bot.js
+++ bot.js
@@ -3,7 +3,23 @@ const bot = new Discord.Client();
 const responseObject = {
     'ziema': 'бля холодно пиздец!',
     'vasara': 'бля холодно пиздец!',
-    'lol': 'roflmaotntpmp'
+    'kaifs': 'Мне по кайфу, когда порнуха в телеке, когда проститутка в "гелике".',
+    'apčī': 'Не чихай, будь здоров! Не забудь сказать А.У.Е. за воров!',
+    'maz': 'В твоей жопе куча мала.',
+    'atceries': 'Помнишь, ебал тебя в бутсах?',
+    'eu': 'Слышь, дядь, вот только без этого, блядь.',
+    'dzirdi': 'Хуй соси!',
+    'notiek': 'Я порвал уздечку в клубе на залупе охранника.',
+    'kāpēc': 'Пойми: тебе не место в стане фристайлеров.',
+    'plāns': 'Копам в жопу нассы!',
+    'uzpīpēt': 'Мои лёгкие чернее, чем твой задний проход.',
+    'hardbass': 'За плечом смерть и коса, в клубах играет "колбаса".',
+    'ahujenna': 'Ахуенно кого-нибудь убить. Просто взять, тупо в голову выстрелить. И похуй что за это могут посадить, просто похуй на все эти мысли',
+    'loliņu': 'ЭТО ВОРОТА АДА ПИЗДЕТЬ УЖЕ НЕ НАДО',
+    'rīt': 'КЕНТЫ ЕДУТ НА ХАТУ К ТЕХНИКУ ПОД СОЛИ И МИКСЫ А ТЫ И ДАЛЬШЕ ХУЙ СОСИ',
+    'Imants': 'ГОНДОН ТЫ ЗЛОЕБУЧИЙ',
+    'darām': 'Меня от соли накрыло!',
+    'sīdžu': 'Жгучая хуйня'
   };
 
 bot.on('ready', () => {
@@ -16,17 +32,8 @@ bot.on('message', (message) => {
     if(responseObject[message.content]) {
         message.channel.send(responseObject[message.content]);
       }
-
     if (message.author.bot) return;
-    
     msg = message.content.toLowerCase();
-
-    if (msg.includes('github')){
-        message.reply('Initialized');
-    }
-    if (msg.includes('ziema','vasara')){
-        message.reply('бля холодно пиздец!');
-    }
 })
 
 bot.login(process.env.BOT_TOKEN);
