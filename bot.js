10ffe164ae68f29a12104b99b6561c030b869ac3
Manacloud <isitamomster@gmail.com>
2018-03-16 21:55:01 +0200

    v0.1

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const args = splice(prefix.length).trim().split(/ +/g);
 
 bot.on('ready', () => {
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
@@ -12,13 +11,12 @@ bot.on('message', (message) => {
 
     if (message.author.bot) return;
     
-    msg = message.content.args.toLowerCase();
+    msg = message.content.toLowerCase();
 
     if (msg.includes('github')){
         message.reply('Initialized');
     }
     if (msg.includes('ziema','vasara')){
-        let [ziema, vasara] = args;
         message.reply('бля холодно пиздец!');
     }
 })
