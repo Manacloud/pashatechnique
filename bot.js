b332c5df89b55b15fd7d7d3a0ef6241bd14c3694
Manacloud <isitamomster@gmail.com>
2018-03-17 16:28:03 +0200

    v0.3

--- bot.js
+++ bot.js
@@ -1,34 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-Array.prototype.toLowerCase = function() { 
-    for (var i = 0; i < this.length; i++) {
-        this[i] = this[i].toString().toLowerCase(); 
-    }
-}
-
-
-const responseObject = {
-    'ziema': 'бля холодно пиздец!',
-    'vasara': 'бля холодно пиздец!',
-    'kaifs': 'Мне по кайфу, когда порнуха в телеке, когда проститутка в "гелике".',
-    'apčī': 'Не чихай, будь здоров! Не забудь сказать А.У.Е. за воров!',
-    'maz': 'В твоей жопе куча мала.',
-    'atceries': 'Помнишь, ебал тебя в бутсах?',
-    'eu': 'Слышь, дядь, вот только без этого, блядь.',
-    'dzirdi': 'Хуй соси!',
-    'notiek': 'Я порвал уздечку в клубе на залупе охранника.',
-    'kāpēc': 'Пойми: тебе не место в стане фристайлеров.',
-    'plāns': 'Копам в жопу нассы!',
-    'uzpīpēt': 'Мои лёгкие чернее, чем твой задний проход.',
-    'hardbass': 'За плечом смерть и коса, в клубах играет "колбаса".',
-    'ahujenna': 'Ахуенно кого-нибудь убить. Просто взять, тупо в голову выстрелить. И похуй что за это могут посадить, просто похуй на все эти мысли',
-    'loliņu': 'ЭТО ВОРОТА АДА ПИЗДЕТЬ УЖЕ НЕ НАДО',
-    'rīt': 'КЕНТЫ ЕДУТ НА ХАТУ К ТЕХНИКУ ПОД СОЛИ И МИКСЫ А ТЫ И ДАЛЬШЕ ХУЙ СОСИ',
-    'Imants': 'ГОНДОН ТЫ ЗЛОЕБУЧИЙ',
-    'darām': 'Меня от соли накрыло!',
-    'sīdžu': 'Жгучая хуйня'
-  };
 
 bot.on('ready', () => {
     console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
@@ -36,11 +8,66 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-
-    if(responseObject[message.content]) {
-        message.channel.send(responseObject[message.content]);
-      }
+    msg = message.content.toLowerCase();
     if (message.author.bot) return;
+
+    if(msg.includes('ziema')){
+        message.reply('бля холодно пиздец!');
+    }
+    if(msg.includes('vasara')){
+        message.reply('бля холодно пиздец!');
+    }
+    if(msg.includes('kaifs')){
+        message.reply('Мне по кайфу, когда порнуха в телеке, когда проститутка в "гелике".');
+    }
+    if(msg.includes('apčī')){
+        message.reply('Не чихай, будь здоров! Не забудь сказать А.У.Е. за воров!');
+    }
+    if(msg.includes('maz')){
+        message.reply('В твоей жопе куча мала.');
+    }
+    if(msg.includes('atceries')){
+        message.reply('Помнишь, ебал тебя в бутсах?');
+    }
+    if(msg.includes('eu')){
+        message.reply('Слышь, дядь, вот только без этого, блядь.');
+    }
+    if(msg.includes('dzirdi')){
+        message.reply('Хуй соси!');
+    }
+    if(msg.includes('notiek')){
+        message.reply('Я порвал уздечку в клубе на залупе охранника.');
+    }
+    if(msg.includes('kāpēc')){
+        message.reply('Пойми: тебе не место в стане фристайлеров.');
+    }
+    if(msg.includes('plāns')){
+        message.reply('Копам в жопу нассы!');
+    }
+    if(msg.includes('uzpīpēt')){
+        message.reply('Мои лёгкие чернее, чем твой задний проход.');
+    }
+    if(msg.includes('hardbass')){
+        message.reply('За плечом смерть и коса, в клубах играет "колбаса".');
+    }
+    if(msg.includes('ahujenna')){
+        message.reply('Ахуенно кого-нибудь убить. Просто взять, тупо в голову выстрелить. И похуй что за это могут посадить, просто похуй на все эти мысли');
+    }
+    if(msg.includes('loliņu')){
+        message.reply('ЭТО ВОРОТА АДА ПИЗДЕТЬ УЖЕ НЕ НАДО');
+    }
+    if(msg.includes('rīt')){
+        message.reply('КЕНТЫ ЕДУТ НА ХАТУ К ТЕХНИКУ ПОД СОЛИ И МИКСЫ А ТЫ И ДАЛЬШЕ ХУЙ СОСИ');
+    }
+    if(msg.includes('Imants')){
+        message.reply('ГОНДОН ТЫ ЗЛОЕБУЧИЙ');
+    }
+    if(msg.includes('darām')){
+        message.reply('Меня от соли накрыло!');
+    }
+    if(msg.includes('sīdžu')){
+        message.reply('Жгучая хуйня');
+    }
 })
 
 bot.login(process.env.BOT_TOKEN);
